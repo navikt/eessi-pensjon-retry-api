d5d9ac16dcc6dbed1eca35a5ef804210a9cc952b
mariampervez <mariam.pervez@nav.no>
2021-10-28 12:19:30 +0200

    -T legger til test
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/test/kotlin/no/nav/eessi/pensjon/retry/RetryKafkaHandlerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/retry/RetryKafkaHandlerTest.kt
@@ -6,7 +6,11 @@ import io.mockk.every
 import io.mockk.mockk
 import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
 import no.nav.eessi.pensjon.security.sts.STSService
+import no.nav.eessi.pensjon.security.sts.typeRef
+import no.nav.eessi.pensjon.utils.mapAnyToJson
+import no.nav.eessi.pensjon.utils.mapJsonToAny
 import no.nav.eessi.pensjon.utils.toJson
+import no.nav.eessi.pensjon.utils.typeRefs
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -42,7 +46,11 @@ internal class RetryIntegrationTest(){
 
     @Test
     fun `Rest-kall til kontroller skal gi en liste tilbake`(){
-        val retList = listOf<String>()
+        val retList = listOf<HendelsModelRetry>(
+            mockHendelse(HendelseType.SENDT),
+            mockHendelse(HendelseType.MOTTATT))
+
+        every { retryService.hentAlleFeiledeHendelser() } returns retList
 
         val result = mockMvc.perform(
             MockMvcRequestBuilders.get("/retrylist")
@@ -54,4 +62,34 @@ internal class RetryIntegrationTest(){
         println(result.response.getContentAsString(charset("UTF-8")))
 
     }
+
+    private fun mockHendelse(hendelseType: HendelseType) : HendelsModelRetry {
+        val mockhendelse = """
+            {
+              "id": 1869,
+              "sedId": "P2000_b12e06dda2c7474b9998c7139c841646_2",
+              "sektorKode": "P",
+              "bucType": "P_BUC_01",
+              "rinaSakId": "147729",
+              "avsenderId": "NO:NAVT003",
+              "avsenderNavn": "NAVT003",
+              "avsenderLand": "NO",
+              "mottakerId": "NO:NAVT007",
+              "mottakerNavn": "NAV Test 07",
+              "mottakerLand": "NO",
+              "rinaDokumentId": "b12e06dda2c7474b9998c7139c841646",
+              "rinaDokumentVersjon": "2",
+              "sedType": "P2000",
+              "navBruker": "09035225916"
+            }
+        """.trimIndent()
+
+        return HendelsModelRetry(mapJsonToAny(mockhendelse, typeRefs()),
+        hendelseType = hendelseType)
+
+
+
+    }
+
+
 }
